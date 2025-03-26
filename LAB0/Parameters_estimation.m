a7f6f64e089c2372220d5199a3b54b3f53992cf9
Luca <>
2025-03-26 22:46:02 +0100

    Add friction estimation

--- LAB0/Parameters_estimation.m
+++ LAB0/Parameters_estimation.m
@@ -1,30 +1,64 @@
-%% 
+% Entry point for parameters estimation. 
+% Just run this file, no need to run the simulations on simulink,
+% everything is automatised.
 
+clear all;
+close all;
+clc;
 
 %% FRICTION ESTIMATOR
-% Computing averages on constant windows
-w_m_acc_mean = friction_window_average(out.w_m_acc, 5, 0, 9, [1, 4], 1);
-w_m_dec_mean = friction_window_average(out.w_m_dec, 5, 45, 9, [1, 4], 1);
+load_system("Friction_Estimation.slx");
+% Positive velocity
+set_param(['Friction_Estimation' '/Manual Switch'], 'sw', '1'); % Set first input
+out = sim("Friction_Estimation.slx");
+w_m_mean = window_average(out.w_m, 5, 9, [1, 4], [], 1);
+tau_m_mean = window_average(out.tau_m, 5, 9, [1, 4], [], 1);
+Phi_T = [w_m_mean (1/gbox.N)*sign(w_m_mean)];
+Y = tau_m_mean;
+thLS_hat = Phi_T\Y;
+Beq_hat_pos = thLS_hat(1);
+tau_sf_pos = thLS_hat(2);
 
-tau_m_acc_mean = friction_window_average(out.tau_m_acc, 5, 0, 9, [1, 4], 1);
-tau_m_dec_mean = friction_window_average(out.tau_m_dec, 5, 45, 9, [1, 4], 1);
+% Negative velocity
+set_param(['Friction_Estimation' '/Manual Switch'], 'sw', '0'); % Set first input
+out = sim("Friction_Estimation.slx");
+w_m_mean = window_average(out.w_m, 5, 9, [1, 4], [], 1);
+tau_m_mean = window_average(out.tau_m, 5, 9, [1, 4], [], 1);
+Phi_T = [w_m_mean (1/gbox.N)*sign(w_m_mean)];
+Y = tau_m_mean;
+thLS_hat = Phi_T\Y;
+Beq_hat_neg = thLS_hat(1);
+tau_sf_neg = thLS_hat(2);
 
-Phi_T_acc = [w_m_acc_mean (1/n)*sign(w_m_acc_mean)];
-Y_acc = [tau_m_acc_mean];
+close_system('Friction_Estimation', 0);
 
-theta_hat_LS_acc = inv(Phi_T_acc' * Phi_T_acc) * Phi_T_acc' * Y_acc;
+Beq = (Beq_hat_pos + Beq_hat_neg)/2
+tau_sf = (abs(tau_sf_pos)+abs(tau_sf_neg))/2
 
-Phi_T_dec = [w_m_dec_mean (1/n)*sign(w_m_dec_mean)];
-Y_dec = [tau_m_dec_mean];
-
-theta_hat_LS_dec = inv(Phi_T_dec' * Phi_T_dec) * Phi_T_dec' * Y_dec;
+%% Not revisioned
+% V_theta_LS_acc = (Y_acc - Phi_T_acc * theta_hat_LS_acc)' * (Y_acc - Phi_T_acc * theta_hat_LS_acc);
+% lambda_square_acc = (1/(9 + 2)) * V_theta_LS_acc;
+% cov_theta_LS_acc = lambda_square_acc * inv(Phi_T_acc' * Phi_T_acc);
+% 
+% V_theta_LS_dec = (Y_dec - Phi_T_dec * theta_hat_LS_dec)' * (Y_dec - Phi_T_dec * theta_hat_LS_dec);
+% lambda_square_dec = (1/(9 + 2)) * V_theta_LS_dec;
+% cov_theta_LS_dec = lambda_square_dec * inv(Phi_T_dec' * Phi_T_dec);
+% 
+% var_Beq_acc = cov_theta_LS_acc(1, 1);
+% var_tau_sf_acc = cov_theta_LS_acc(2, 2);
+% 
+% var_Beq_dec = cov_theta_LS_dec(1, 1);
+% var_tau_sf_dec = cov_theta_LS_dec(2, 2);
+% 
+% c = 1.96;
+% Beq_Interval_Dec = Beq + c * sqrt(var_Beq_dec)*[-1, 1]
+% Beq_Interval_Acc = Beq + c * sqrt(var_Beq_acc)*[-1, 1]
+% 
+% Tau_sf_Interval_Dec = tau_sf + c * sqrt(var_tau_sf_dec)*[-1, 1]
+% Tau_sf_Interval_Acc = tau_sf + c * sqrt(var_tau_sf_acc)*[-1, 1]
 
 %% Inertia estimation
-% Substitute these with the estimated values when available
-Beq = 2.0e-6;
-tau_sf = 1.0e-2;
-
-% Run simulation
+load_system("Inertia_Estimation.slx");
 out = sim("Inertia_Estimation.slx");
 
 % Computing averages on constant windows
@@ -37,6 +71,8 @@ avg = 0;
 for k = 1:N_periods
     new = ( (tau_i_mean(k,1) - tau_i_mean(k,2)) / (a_m_mean(k,1) - a_m_mean(k,2)) );
     avg = avg + new;
-    disp(new);
 end
-Jeq_hat = avg/N_periods;
+
+Jeq_hat = avg/N_periods
+
+close_system('Inertia_Estimation', 0);
