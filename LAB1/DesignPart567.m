dee81cb60b0d08d49790e56a82bae453911031d5
Banaan Kiamanesh <kiamaneshbanaan@gmail.com>
2025-04-02 15:01:12 +0200

    Fixing Problems with the Conflicts

--- LAB1/DesignPart567.m
+++ LAB1/DesignPart567.m
@@ -6,6 +6,22 @@ clc
 Init
 addpath('../LAB0/')
 
+%% Primary Parameter Declaration
+% Estimated Parameters
+Jeq_hat = 5.801020129074022e-05;      % Equivalent Inertia [kg.m^2]
+Beq = 1.223604206496999e-06;          % Equivalent Viscous Friction [Nm/(mot.Rd/s)]
+Tau_sf = 0.005709536387019;           % Static Friction [Nm]
+
+% Recalculating Constants
+Tm = (mot.Req * Beq + mot.Kt*mot.Ke) / (mot.Req * Jeq_hat);
+km = (drv.dcgain * mot.Kt) / (gbox.N * mot.Req * Jeq_hat);
+
+% State-Space Matrices
+A = [0, 1; 0, -1/Tm];
+B = [0, km/(gbox.N*Tm)]';
+C = [1, 0];
+D = 0;
+
 %% Design and Test
 % Desired Second Order System Characteristics
 Mp = 0.1;           % Over Shoot Percent
--- LAB1/Init.m
+++ LAB1/Init.m
@@ -110,21 +110,6 @@ Ts = 0.001;                                     % Sampling Time
 delta_est = 1/sqrt(2);
 wc = 2*pi*20;
 
-%% Estimated Parameters
-Jeq_hat = 5.801020129074022e-05;      % Equivalent Inertia [kg.m^2]
-Beq = 1.223604206496999e-06;          % Equivalent Viscous Friction [Nm/(mot.Rd/s)]
-Tau_sf = 0.005709536387019;           % Static Friction [Nm]
-
-%% Recalculating Constants
-Tm = (mot.Req * Beq + mot.Kt*mot.Ke) / (mot.Req * Jeq_hat);
-km = (drv.dcgain * mot.Kt) / (gbox.N * mot.Req * Jeq_hat);
-
-%% State-Space Matrices
-A = [0, 1; 0, -1/Tm];
-B = [0, km/(gbox.N*Tm)]';
-C = [1, 0];
-D = 0;
-
 % % Transfer Function
 % TFModel = tf(ss(A, B, C, D));
 
@@ -142,38 +127,34 @@ Jl = mld .JD + 3* gbox .J72 ;
 Jeq = mot.J + (Jl / gbox .N1 ^2) ;
 %Jeq = 5.801020129074022e-05;
 Beq = 1.223604206496999e-06;
-wn = 3/(delta * ts);
+
+wn = 3/(delta_est * ts);
 %delta2 = log(1/mp) / sqrt((pi*pi) +(log(1/mp)*log(1/mp)));
 
 %% high passs ("real derivative")
-delta_h1 = 1/sqrt(2);
 wc = 2*pi*50;
 
 %% State Space
 Tm = (mot.Req * Jeq)/(mot.Req*Beq + mot.Kt*mot.Ke);
-A = [[0 1 ],
-    [0 -1/Tm]];
-B = [0,
-    drv.dcgain*mot.Kt/(gbox.N1*mot.Req*Jeq)];
-C = [1 0];
+A = [0, 1;0, -1/Tm];
+B = [0; drv.dcgain*mot.Kt/(gbox.N1*mot.Req*Jeq)];
+C = [1, 0];
 D = 0;
 
-M = [[A B],
-    [C D]];
-M_inv = inv(M);
-Ans = M_inv * [0;0;1];
+M = [A, B;C, D];
+Ans = M \ [0; 0; 1];
 Nx = Ans(1:2,1);
 Nu = Ans(3,1);
-pole1 =(-delta * wn) + j*wn*sqrt(1-delta^2);
-pole2 = (-delta * wn) - j*wn*sqrt(1-delta^2);
+pole1 = (-delta_est * wn) + 1j*wn*sqrt(1-delta_est^2);
+pole2 = (-delta_est * wn) - 1j*wn*sqrt(1-delta_est^2);
 poles = [pole1,pole2];
 K = acker(A,B,poles);
-Nr=Nu+K * Nx;
+Nr = Nu + K*Nx;
 
 %% part 2
-sigma = real ( pole1 );
-omega_d = imag ( pole1 );
-poles2 = [sigma+1i*omega_d sigma-1i*omega_d sigma ];
+sigma = real(pole1);
+omega_d = imag(pole1);
+% poles2 = [sigma+1i*omega_d sigma-1i*omega_d sigma ];
 poles2 = sigma * ones (3 ,1);
 %poles2 = [2*sigma+1i*omega_d 2*sigma-1i*omega_d 2* sigma ];
 %poles2 = [2*sigma+1i*omega_d 2*sigma-1i*omega_d 3*sigma ];
@@ -184,25 +165,3 @@ Ce =[0 C];
 K_robust  = acker (Ae , Be , poles2 );
 KI = K_robust(1);
 K2 = K_robust(2:3);
-
-%% plotting with different poles
-load('first.mat');
-load('second.mat');
-load('third.mat');
-load('fourth.mat');
-figure;
-hold on;
-title('40 Step');
-plot(first.time,first.signals.values)
-plot(first.time,second.signals.values)
-plot(first.time,third.signals.values)
-plot(first.time,fourth.signals.values)
-legend({'First', 'Second','Third','Fourth'}, 'Location', 'best');
-figure;
-hold on;
-title('40 Step Error');
-plot(first.time,40-first.signals.values)
-plot(first.time,40-second.signals.values)
-plot(first.time,40-third.signals.values)
-plot(first.time,40-fourth.signals.values)
-legend({'First', 'Second','Third','Fourth'}, 'Location', 'best');
