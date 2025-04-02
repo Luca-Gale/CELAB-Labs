77f3bf6dafbe0350df290228ed25dc5c24edba5b
Edoardo Sipari <edoardosipari27@gmail.com>
2025-04-02 21:33:25 +0200

    WIP: PID in LAB1 file

--- LAB1/Init.m
+++ LAB1/Init.m
@@ -115,9 +115,35 @@ wc = 2*pi*20;
 
 %% PID wind-up parameters
 ts = 0.15;
+mp    = 0.1;
+alpha = 84;
+delta = log(1/mp) / ...
+        sqrt(pi^2 + (log(1/mp))^2);
+
+wgc = 3 / (delta * ts);
+phim = atan2(2 * delta, ...
+             sqrt(sqrt(1 + 4*delta^4) - 2*delta^2));
+
 Tw = ts/5;
 Kw = 1/Tw;
+% Transfer function
+P = km / ...
+    (gbox.N*1i*wgc * (Tm*1i*wgc + 1));
 
+% Controller Bode Gain
+abs_P = abs(P);
+deltak = 1/abs_P;
+deltaphi = -pi + phim - angle(P);
+% Controller Gains
+Kp = deltak * cos(deltaphi);
+tanphi = tan(deltaphi);
+Td = (tanphi + sqrt(tanphi^2 + 4/alpha)) / ...
+     (2*wgc);
+Ti = alpha * Td;
+Kd = Kp * Td;
+Ki = Kp / Ti;
+
+tl = 1/(3*wgc);
 %% feedforward compensation parameters
 k_drv = (1 + drv.R3/drv.R4) * drv.R2/(drv.R1 + drv.R2);
 
