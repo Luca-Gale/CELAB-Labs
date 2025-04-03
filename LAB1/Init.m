5ef8ab6236661a45996f7950fa659f63e6741b12
Edoardo Sipari <edoardosipari27@gmail.com>
2025-04-03 10:32:10 +0200

    WIP: Feedforward

--- LAB1/Init.m
+++ LAB1/Init.m
@@ -157,7 +157,12 @@ Ki = Kp / Ti;
 tl = 1/(3*wgc);
 %% feedforward compensation parameters
 k_drv = (1 + drv.R3/drv.R4) * drv.R2/(drv.R1 + drv.R2);
-
+%inertia comp
+I_C = (gbox.N * mot.Req * Jeq_hat)/(k_drv  * mot.Kt);
+%friction comp
+F_C = mot.Req/(k_drv * mot.Kt * gbox.N);
+%BEMF comp
+B_C = (gbox.N * mot.Ke)/k_drv;
 %% load data
 Tau_sf = 0.005709536387019;
 Jl = mld .JD + 3* gbox .J72 ;
