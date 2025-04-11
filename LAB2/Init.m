80430e504169e7adce0692f511130a35822c7942
Luca <>
2025-04-11 18:41:43 +0200

    Part 1-2-5 LAB2

--- LAB2/Init.m
+++ LAB2/Init.m
@@ -10,10 +10,17 @@ Ts = 1e-3;
 Smax = 0.1e-3;
 
 %% Discrete time PID design
+
+% Load Kd, Kp, Ki, tl, wgc
 load('PID_gains_with_estimated_params');
+
+% Load Beq, Jeq_hat, tau_sf
 load('black-box-estimation.mat');
-% Time constant for the real-derivative
-TL = 1/(2*wgc);
+
+% Real-derivative transfer function
+TL = 1/(2*wgc*10); % Multiply for 10 seems to be working soo much better. Need more testing
+num = [1 -1];
+den = [(TL+Ts) -TL];
 
 k_drv = (1 + drv.R3/drv.R4) * drv.R2/(drv.R1 + drv.R2);
 %inertia comp
@@ -22,3 +29,20 @@ I_C = (gbox.N * mot.Req * Jeq_hat)/(k_drv  * mot.Kt);
 F_C = mot.Req/(k_drv * mot.Kt * gbox.N);
 %BEMF comp
 B_C = (gbox.N * mot.Ke)/k_drv;
+
+%% PID with different discretization methods
+% CT transfer function
+s = tf('s');
+C = Kp+Ki/s+Kd*s/(1+TL*s);
+
+% Variable of DT transfer function
+z = tf('z', Ts);
+% Tustin method
+s = 2/Ts*(z-1)/(z+1);
+C_T = minreal(Kp+Ki/s+Kd*s/(1+TL*s));
+[num_T, den_T] = tfdata(C_T, 'v');
+
+% Forward method
+s = (z-1)/Ts;
+C_FE = minreal(Kp+Ki/s+Kd*s/(1+TL*s));
+[num_FE, den_FE] = tfdata(C_FE, 'v');
