d99a0bee580e8f1695b2034967e274f36ce67d8d
k4ktoos <Alidarvishi8520@gmail.com>
2025-04-20 16:23:51 +0200

    WIP : Report

--- Lab_17_04/LAB2/SAVED_RESULTS/ali_report.m
+++ Lab_17_04/LAB2/SAVED_RESULTS/ali_report.m
@@ -8,12 +8,34 @@ T50prof=load('ali_proflambda_50ms.mat');
 
 figure;
 plot(T1.ali_50ms.time,T1.ali_50ms.signals(1).values)
+grid on;
+xlabel('X-axis: Time (s)');
+ylabel('Y-axis: Position');
+title('Ts = 1ms');
+
 
 figure;
 plot(T10.ali_50ms.time,T10.ali_50ms.signals(1).values)
+grid on;
+xlabel('X-axis: Time (s)');
+ylabel('Y-axis: Position');
+title('Ts = 10ms');
+% legend();
+
 
 figure;
 plot(T50ali.ali_50ms.time,T50ali.ali_50ms.signals(1).values)
+grid on;
+xlabel('X-axis: Time (s)');
+ylabel('Y-axis: Position');
+title('Ts = 50ms');
+% legend();
+
 
 figure;
 plot(T50prof.ali_50ms.time,T50prof.ali_50ms.signals(1).values)
+grid on;
+xlabel('X-axis: Time (s)');
+ylabel('Y-axis: Position');
+title('Ts = 50ms');
+% legend();
