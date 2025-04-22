5176b9bbd2102c877fd551f4e31dbc481ae5a529
k4ktoos <Alidarvishi8520@gmail.com>
2025-04-22 18:36:23 +0200

    Report LAB2

--- Lab_17_04/LAB2/SAVED_RESULTS/ali_report.m
+++ Lab_17_04/LAB2/SAVED_RESULTS/ali_report.m
@@ -1,46 +1,60 @@
-clc;
-clear;
+% clc;
+% clear;
 
-T1=load('ali_newlambda_1ms.mat');
-T10=load('ali_newlambda_10ms.mat');
-T50ali=load('ali_newlambda_50ms.mat');
-T50prof=load('ali_proflambda_50ms.mat');
-
-idx = T1.ali_50ms.time < 1.5;
-idx1 = T10.ali_50ms.time < 1.5;
-idx2 = T50ali.ali_50ms.time < 1.5;
-idx3 = T50prof.ali_50ms.time < 1.5;
-
-figure;
-stairs(T1.ali_50ms.time(idx),T1.ali_50ms.signals(1).values(idx))
-grid on;
-xlabel('X-axis: Time (s)');
-ylabel('Y-axis: Position');
-title('Ts = 1ms');
-
-
-figure;
-stairs(T10.ali_50ms.time(idx1),T10.ali_50ms.signals(1).values(idx1))
-grid on;
-xlabel('X-axis: Time (s)');
-ylabel('Y-axis: Position');
-title('Ts = 10ms');
-% legend();
-
-
-figure;
-stairs(T50ali.ali_50ms.time(idx2),T50ali.ali_50ms.signals(1).values(idx2))
-grid on;
-xlabel('X-axis: Time (s)');
-ylabel('Y-axis: Position');
-title('Ts = 50ms');
-% legend();
-
+%% Real-time LAB2
+% T1=load('ali_newlambda_1ms.mat');
+% T10=load('ali_newlambda_10ms.mat');
+% T50ali=load('ali_newlambda_50ms.mat');
+% T50prof=load('ali_proflambda_50ms.mat');
+% 
+% idx = T1.ali_50ms.time < 2;
+% idx1 = T10.ali_50ms.time < 2;
+% idx2 = T50ali.ali_50ms.time < 2;
+% idx3 = T50prof.ali_50ms.time < 2;
+% 
+% figure;
+% stairs(T1.ali_50ms.time(idx),T1.ali_50ms.signals(1).values(idx))
+% grid on;
+% xlabel('Time');
+% ylabel('Position');
+% title('Ts = 1ms');
+% 
+% 
+% figure;
+% stairs(T10.ali_50ms.time(idx1),T10.ali_50ms.signals(1).values(idx1))
+% grid on;
+% xlabel('Time');
+% ylabel('Position');
+% title('Ts = 10ms');
+% % legend();
+% 
+% 
+% figure;
+% stairs(T50ali.ali_50ms.time(idx2),T50ali.ali_50ms.signals(1).values(idx2))
+% grid on;
+% xlabel('Time');
+% ylabel('Position');
+% title('Ts = 50ms');
+% % legend();
+% 
 % 
 % figure;
 % stairs(T50prof.ali_50ms.time(idx3),T50prof.ali_50ms.signals(1).values(idx3))
 % grid on;
-% xlabel('X-axis: Time (s)');
-% ylabel('Y-axis: Position');
+% xlabel('Time');
+% ylabel('Position');
 % title('Ts = 50ms');
+% % legend();
+
+
+%% simulation LAB2
+figure;
+% stairs(T50prof.ali_50ms.time(idx3),T50prof.ali_50ms.signals(1).values(idx3))
+stairs(out.final3.time,out.final3.signals.values)
+% xticks(0:0.2:5);    % Smaller spacing on x-axis
+% yticks(0:10:150); 
+grid on;
+xlabel('Time');
+ylabel('Position');
+title('Discrete time, Ts = 10ms');
 % % legend();
