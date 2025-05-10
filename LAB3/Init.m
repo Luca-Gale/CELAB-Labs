129261591c80f295d60b3fa1b8cd3a625a00b07b
Luca <>
2025-05-10 18:57:10 +0200

    Resonant load handmade model

---
+++ LAB3/Init.m
@@ -0,0 +1,12 @@
+clear all;
+close all;
+clc;
+
+%% Load datasheet
+addpath('..\..\CELAB-Labs\utilities\');
+datasheet;
+datasheet_resonant;
+
+%% Beq, Jeq_hat, tau_sf
+load('..\utilities\black-box-estimation.mat');
+
