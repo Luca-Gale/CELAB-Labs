0f47aa1e04a3f24e41c44bee60a55559b5dddd42
k4ktoos <Alidarvishi8520@gmail.com>
2025-05-26 21:53:00 +0200

    Result and plotting

--- LAB4/S6 - S10/plotting.m
+++ LAB4/S6 - S10/plotting.m
@@ -1,8 +1,17 @@
 %% plotting
-
+figure(1);
 plot(out.final.time,out.final.signals.values)
 % title('S8-1');
 xlabel('Time');
 ylabel('Degree');
-ylim([-30 30]);
+ylim([-50 50]);
+grid on;
+
+
+figure(2);
+plot(out.final.time,out.final.signals.values)
+% title('S8-1');
+xlabel('Time');
+ylabel('Degree');
+ylim([-15 15]);
 grid on;
